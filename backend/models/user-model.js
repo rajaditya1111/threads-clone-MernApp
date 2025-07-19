4d8ea31164485f64d24736e6a8cb2be145727710
rajaditya1111 <aditya171219raj@gmail.com>
2025-07-20 02:30:00 +0530

    Add files via upload

---
+++ backend/models/user-model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+
+//trim = For no extra-spaces
+//select = false {this info. will not go with API like other data}
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    bio: {
+      type: String,
+    },
+
+    profilePic: {
+      type: String,
+      default:
+        "https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Clipart.png",
+    },
+
+    public_id: {
+      type: String,
+    },
+    
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
+    threads: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
+    replies: [{ type: mongoose.Schema.Types.ObjectId, ref: "comment" }],
+    reposts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("user", userSchema);
